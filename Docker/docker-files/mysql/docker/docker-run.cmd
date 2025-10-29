eda746d0b8cdea6454f88c71baa332346185bf3f
Christhian <peralta6998@gmail.com>
2025-10-29 16:50:55 -0500

    Correcciones basicas y docker compose, ejecutar con docker-compose up -d

--- Docker/docker-files/mysql/docker/docker-run.cmd
+++ Docker/docker-files/mysql/docker/docker-run.cmd
@@ -5,4 +5,4 @@ REM Crear el network
 docker network create proyectoDAWII
 
 REM Creamos un contenedor desde la imagen oficial de MySQL
-docker run -d --name mysql-proyectoDAWII --network proyectoDAWII -p 3307:3306 -v mysql-data:/var/lib/mysql -v ..\..\util\script\script.sql:/docker-entrypoint-initdb.d/init.sql:ro -e MYSQL_ROOT_PASSWORD=sql mysql:latest
+docker run -d --name mysql-proyectoDAWII --network proyectoDAWII -p 3307:3306 -v mysql-data:/var/lib/mysql -v .\Docker\docker-files\util\script\script.sql:/docker-entrypoint-initdb.d/init.sql:ro -e MYSQL_ROOT_PASSWORD=sql mysql:latest
